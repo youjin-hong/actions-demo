b3dfe2926d00cd9f6ca45ae0aeeec53d76e83f42
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-11-12 14:18:59 +0900

    [demo] assertion 추가

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,13 @@ module.exports = {
     collect: {
       staticDistDir: "./build",
       url: ["http://localhost:3000"],
-      numberOfRuns: 5,
+      numberOfRuns: 2,
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 1 }],
+      },
     },
     upload: {
       target: "temporary-public-storage",
